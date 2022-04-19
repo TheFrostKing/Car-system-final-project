5f492d37f6f63ee9974f3ef74086a7be5a6faf01
TheFrostKing <37861327+TheFrostKing@users.noreply.github.com>
2022-04-19 22:37:34 +0200

    Add files via upload

---
+++ Car-System/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace Car_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btnHazard = new System.Windows.Forms.Button();
+            this.lsbBlackBox = new System.Windows.Forms.ListBox();
+            this.txtTemp = new System.Windows.Forms.TextBox();
+            this.txtHeadlights = new System.Windows.Forms.TextBox();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.lblHeadlights = new System.Windows.Forms.Label();
+            this.timerAnimate = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.PortName = "COM5";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btnHazard
+            // 
+            this.btnHazard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnHazard.Location = new System.Drawing.Point(39, 46);
+            this.btnHazard.Name = "btnHazard";
+            this.btnHazard.Size = new System.Drawing.Size(104, 64);
+            this.btnHazard.TabIndex = 0;
+            this.btnHazard.Text = "HAZARD";
+            this.btnHazard.UseVisualStyleBackColor = false;
+            this.btnHazard.BackColorChanged += new System.EventHandler(this.timer_Tick);
+            this.btnHazard.Click += new System.EventHandler(this.btnHazard_Click);
+            // 
+            // lsbBlackBox
+            // 
+            this.lsbBlackBox.FormattingEnabled = true;
+            this.lsbBlackBox.Location = new System.Drawing.Point(553, 55);
+            this.lsbBlackBox.Name = "lsbBlackBox";
+            this.lsbBlackBox.Size = new System.Drawing.Size(147, 277);
+            this.lsbBlackBox.TabIndex = 1;
+            // 
+            // txtTemp
+            // 
+            this.txtTemp.Location = new System.Drawing.Point(344, 122);
+            this.txtTemp.Name = "txtTemp";
+            this.txtTemp.Size = new System.Drawing.Size(100, 20);
+            this.txtTemp.TabIndex = 2;
+            // 
+            // txtHeadlights
+            // 
+            this.txtHeadlights.Location = new System.Drawing.Point(344, 184);
+            this.txtHeadlights.Name = "txtHeadlights";
+            this.txtHeadlights.Size = new System.Drawing.Size(100, 20);
+            this.txtHeadlights.TabIndex = 3;
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Location = new System.Drawing.Point(282, 122);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(34, 13);
+            this.lblTemp.TabIndex = 4;
+            this.lblTemp.Text = "Temp";
+            // 
+            // lblHeadlights
+            // 
+            this.lblHeadlights.AutoSize = true;
+            this.lblHeadlights.Location = new System.Drawing.Point(259, 187);
+            this.lblHeadlights.Name = "lblHeadlights";
+            this.lblHeadlights.Size = new System.Drawing.Size(57, 13);
+            this.lblHeadlights.TabIndex = 5;
+            this.lblHeadlights.Text = "Headlights";
+            // 
+            // timerAnimate
+            // 
+            this.timerAnimate.Interval = 1000;
+            this.timerAnimate.Tick += new System.EventHandler(this.timerAnimate_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHeadlights);
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.txtHeadlights);
+            this.Controls.Add(this.txtTemp);
+            this.Controls.Add(this.lsbBlackBox);
+            this.Controls.Add(this.btnHazard);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btnHazard;
+        private System.Windows.Forms.ListBox lsbBlackBox;
+        private System.Windows.Forms.TextBox txtTemp;
+        private System.Windows.Forms.TextBox txtHeadlights;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.Label lblHeadlights;
+        private System.Windows.Forms.Timer timerAnimate;
+    }
+}
+
---
+++ Car-System/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_System
+{
+    public partial class Form1 : Form
+    {
+        bool isItOn = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Set up the serial port.
+            serialPort.PortName = "COM5";
+            serialPort.DtrEnable = true; // For Arduino Leonardo
+            serialPort.Open();
+            // Set up the timers.
+            timer.Start();
+           
+        }
+
+      
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Close the timers.
+            timer.Stop();
+            timerAnimate.Stop();
+            // Release the serial port.
+            serialPort.Close();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if(serialPort.BytesToRead>0)
+            {
+                string message = serialPort.ReadLine();
+                if (message.Trim()=="HdlON")
+                {
+                    txtHeadlights.Text = message.Remove(0,3);
+                    lsbBlackBox.Items.Add("Lights: " + message.Remove(0, 3));
+                    Refresh();
+                }
+                if (message.Trim() == "HdlOFF")
+                {
+                    txtHeadlights.Text = message.Remove(0, 3);
+                    lsbBlackBox.Items.Add("Lights: " + message.Remove(0,3));
+                    Refresh();
+                }
+                if (message.Trim().StartsWith("T"))
+                {
+                    
+                    lsbBlackBox.Items.Add(message+" C");
+                    txtTemp.Text= message.Remove(0, 5)+" C";
+                    Refresh();
+                }
+                if (message.Trim()=="Left")
+                {
+                    
+                    lsbBlackBox.Items.Add(message);
+                    Refresh();
+                }
+                if (message.Trim() == "Right")
+                {
+                    lsbBlackBox.Items.Add(message);
+                    Refresh();
+                }
+            }
+        }
+        private void btnHazard_Click(object sender, EventArgs e)
+        {
+            if (!isItOn)
+            {
+                timerAnimate.Start();
+                isItOn = true;
+                serialPort.Write("HAZARD\n");
+                lsbBlackBox.Items.Add("Hazard");
+                Refresh();
+            }
+            else
+            {
+                timerAnimate.Stop();
+                btnHazard.BackColor = Color.Gray;
+                isItOn = false;
+                serialPort.Write("NORMAL\n");
+                lsbBlackBox.Items.Add("Normal");
+                Refresh();
+            }
+        }
+
+        private void timerAnimate_Tick(object sender, EventArgs e)
+        {
+            if (btnHazard.BackColor == Color.Gray)
+            {
+                btnHazard.BackColor = Color.Red;
+            }
+            else
+            {
+                btnHazard.BackColor = Color.Gray;
+            }
+        }
+    }
+}
